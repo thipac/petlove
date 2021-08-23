c1cba3457685eea8b361a20ccb0683cac095e452
thipac <thiagolmcpacheco@gmail.com>
2021-08-22 21:51:02 -0300

    Arrumando os caminhos do Components

--- src/Components/Dogs/index.jsx
+++ src/Components/Dogs/index.jsx
@@ -1,25 +1,26 @@
-import React from 'react' //rfce 
+import React, { useState, useEffect } from 'react' //rfce 
 import * as S from "./Dogs.styles"
 
 const Dogs = () => {
 
     const [dadosApi, setDadosApi] = useState([])
+    const [apiChegou, setApiChegou] = useState(false)
     
     useEffect(() => {
         fetch('https://nameless-escarpment-53355.herokuapp.com/pets/dogs')
-        .then((resposta) => res.json())
-        .then((dados) => setDadosApi(dados))
+        .then((resposta) => resposta.json())
+        .then((dados) => {
+            setDadosApi(dados.results)
+            setApiChegou(true)
         }, [])
     
     return (
         <S.Container>
-             { !! dadosApi ? (
-                 dadosApi.map((item)=>{
-                     console.log(item.nome);
-                 })
-             ) : (<p>Carregando</p>)}
+             {!!dadosApi && apiChegou ? (dadosApi.map((item) => console.log(item.nome))) : (<p>Carregando</p>)}
         </S.Container>
-    )
-}
+        )    
+    })
+}  
+
 
 export default Dogs
--- src/Pages/Pets/index.jsx
+++ src/Pages/Pets/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react' //rfce 
-import Dogs from '../../Components/Dogs'
-import * as S from "./Pets.styles"
+import React from 'react'; //rfce 
+import Dogs from '../../Components/Dogs';
+import * as S from "./Pets.styles";
 
 const Pets = () => {
     return (
